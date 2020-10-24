49de509c2ac3352570cda72b34e4f1fe0b294832
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-24 17:23:54 +0900

    add.TimeZone_setting

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module Kamigram
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.generators do |g|
       g.assets false
