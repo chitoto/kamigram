74f0f109db12a5291fff1610057604186c933aba
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-11-02 04:27:17 +0900

    fixed:UI_fixed:seed.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,5 @@ class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
+  has_many :favorite_user, through: :favorites, source: :user
 end
