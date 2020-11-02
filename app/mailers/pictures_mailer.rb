03bd521e1787c6719c65c1c8e87a6c1d21056133
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-11-02 23:06:57 +0900

    heroku_make it better

--- app/mailers/pictures_mailer.rb
+++ app/mailers/pictures_mailer.rb
@@ -1,6 +1,6 @@
 class PicturesMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: "tototina.ruru@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "tototina.ruru@gmail.com", subject: "投稿の確認メール"
   end
 end
